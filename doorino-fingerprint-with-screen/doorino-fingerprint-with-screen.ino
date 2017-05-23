696ec684f09689a63c9ca93302b690ac4553d7e2
droberin <drober@gmail.com>
2017-05-23 23:28:27 +0200

    First stable Fingerprint + display version

--- doorino-fingerprint-with-screen/doorino-fingerprint-with-screen.ino
+++ doorino-fingerprint-with-screen/doorino-fingerprint-with-screen.ino
@@ -1,3 +1,11 @@
+/*
+ * Find this source at https://github.com/droberin/doorino-fingerprint
+ * Created by Roberto Salgado <drober+arduino@gmail.com>
+ * it's based on Adafruit's Display and Fingerprint Libraries.
+ * 
+ * Purpose: close a relay circuit using a fingerprint reader to unlock a door
+ */
+
 /*********************************************************************
 This is an example for our Monochrome OLEDs based on SSD1306 drivers
 
@@ -16,72 +24,183 @@ BSD license, check license.txt for more information
 All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
 
+#define RELAY1 12
+#define RELAY_OFF LOW
+#define RELAY_ON HIGH
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+int getFingerprintIDez();
+
+
+
+
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+SoftwareSerial mySerial(8, 9);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
+//Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1);
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
-
-void setup()   {                
-  Serial.begin(9600);
-
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done
-
-  // Show Adafruit logo as demanded in library license
-  // might be deactivated while in dev mode. Sorry, folks.
-  //display.display();
-  //delay(1000);
-  display.clearDisplay();
-
-  // draw a single pixel
-  display.drawPixel(10, 10, WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  displayAccess(2, 3);
-  delay(20000);
-
-}
-
-
-void loop() {
-}
-
-void displayAccess(int user_id, int matching) {
-  char message_line1[16];
-  char message_line2[16];
+void displayMessage(char message[128]) {
+  display.stopscroll();
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Fingerprint access\nSystem v0.1");
-  sprintf(message_line1, "Accepted ID #%i", user_id);
-  sprintf(message_line2, "Matching: %i", matching);
-  display.println(message_line1);
-  display.println(message_line2);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  //display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
+  display.setCursor(1,0);
+  display.println("Fingerprint access\nSystem v0.1\n");
+  display.println(message);
   display.display();
-  delay(12500);
+}
+
+void setup()
+{
+  pinMode(RELAY1, OUTPUT);
+  digitalWrite(RELAY1, RELAY_OFF);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+
+  Serial.begin(9600);
+  Serial.println("Adafruit finger detect test");
+  displayMessage("Looking for fingerprint sensor");
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+      displayMessage("System's fucked");
+    while (1);
+  }
+  Serial.println("Waiting for valid finger...");
+  displayMessage("System's ready");
+  delay(1500);
+  screensaver();
+}
+
+void loop()                     // run over and over again
+{
+  getFingerprintIDez();
+  delay(50);            //don't ned to run this at full speed.
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  char initialMessage[13] = "Welcome, mate";
+  char welcomeMessage[18];
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+//  Serial.print("Found ID #"); Serial.print(finger.fingerID);
+//  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  if (finger.confidence > 70) {
+    sprintf(welcomeMessage, "%s = %i", initialMessage,finger.fingerID);
+    displayMessage(welcomeMessage);
+    openGate(4000);
+  }
+  return finger.fingerID;
+}
+
+void screensaver(void) {
   display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10,00);
+  display.clearDisplay();
+  display.println("Dragon's Spirit");
+  display.display();
+  display.startscrollleft(0x00, 0x0F);
+}
+
+
+void openGate(int duration)
+{
+  digitalWrite(RELAY1, HIGH);
+  delay(duration);
+  digitalWrite(RELAY1, LOW);
+  screensaver();
 }
 
